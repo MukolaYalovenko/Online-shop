d3d7f79c3126e9096ad2e9c70b77d4fa2c734f84
Mukola <k.yalovenko1992@ukr.net>
2023-01-12 10:14:04 +0200

    add likes js

--- app.js
+++ app.js
@@ -7,7 +7,26 @@ let addToCartBtns = document.querySelectorAll(".btn-add-to-cart");
 for (let i = 0; i < addToCartBtns.length; i++) {
   addToCartBtns[i].addEventListener("click", function () {
     // console.log("clicked");
+    //   варіант 1
+    // productsCountEl.textContent = +productsCountEl.textContent + 1;
+    // варіант 2
     let prevProductsCount = +productsCountEl.textContent;
     productsCountEl.textContent = prevProductsCount + 1;
   });
 }
+
+//   Задача про лайки
+
+let likeBtns = document.querySelectorAll(".like");
+console.log(likeBtns);
+
+likeBtns.forEach((btn) =>
+  btn.addEventListener("click", function () {
+    // console.log("cliked");
+    if (btn.classList.contains("liked")) {
+      btn.classList.remove("liked");
+    } else {
+      btn.classList.add("liked");
+    }
+  })
+);
